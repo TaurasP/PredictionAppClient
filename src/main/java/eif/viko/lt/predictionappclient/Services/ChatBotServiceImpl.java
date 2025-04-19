a011de29fdd5842e17138021f003ceef58e9934f
Tauras Petrauskas <petrauskas.post@gmail.com>
2025-04-19 23:25:03 +0300

    Updated chatbot answers by category hyperlinks list.

--- src/main/java/eif/viko/lt/predictionappclient/Services/ChatBotServiceImpl.java
+++ src/main/java/eif/viko/lt/predictionappclient/Services/ChatBotServiceImpl.java
@@ -44,12 +44,24 @@ public class ChatBotServiceImpl {
 
     private String getAnswerByCategory(String category) {
         return switch (category) {
-            case "__label__syntax" -> "https://www.w3schools.com/java/java_syntax.asp";
-            case "__label__oop" -> "https://www.w3schools.com/java/java_oop.asp";
+            case "__label__variables" -> "https://www.w3schools.com/java/java_variables.asp";
+            case "__label__inheritance" -> "https://www.w3schools.com/java/java_inheritance.asp";
             case "__label__exception" -> "https://www.w3schools.com/java/java_try_catch.asp";
             case "__label__rest" -> "https://www.tutorialspoint.com/spring_boot/spring_boot_building_restful_web_services.htm";
             case "__label__spring" -> "https://www.tutorialspoint.com/spring_boot/index.htm";
             case "__label__method" -> "https://www.w3schools.com/java/java_methods.asp";
+            case "__label__constructor" -> "https://www.w3schools.com/java/java_constructors.asp";
+            case "__label__polymorphism" -> "https://www.w3schools.com/java/java_polymorphism.asp";
+            case "__label__abstract" -> "https://www.w3schools.com/java/java_abstract.asp";
+            case "__label__lambda" -> "https://www.w3schools.com/java/java_lambda.asp";
+            case "__label__comment" -> "https://www.w3schools.com/java/java_comments.asp";
+            case "__label__types" -> "https://www.w3schools.com/java/java_data_types.asp";
+            case "__label__arrays" -> "https://www.w3schools.com/java/java_arrays.asp";
+            case "__label__threads" -> "https://www.w3schools.com/java/java_threads.asp";
+            case "__label__while" -> "https://www.w3schools.com/java/java_while_loop.asp";
+            case "__label__for" -> "https://www.w3schools.com/java/java_for_loop.asp";
+            case "__label__switch" -> "https://www.w3schools.com/java/java_switch.asp";
+            case "__label__if" -> "https://www.w3schools.com/java/java_conditions.asp";
             default -> "No category found";
         };
     }
